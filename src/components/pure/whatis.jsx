1ef590d3d8cbfdc22c19d5576ab8876c38ac5d21
javtr <javtriana.z@gmail.com>
2022-10-09 23:05:46 -0500

    ajustes

--- src/components/pure/whatis.jsx
+++ src/components/pure/whatis.jsx
@@ -6,8 +6,8 @@ import whatImg2 from '../../assets/img/otm_02.png'
 export default function Whatis() {
   return (
     <div className="what">
-      <h2 className="global__tx-lg  what__title">What is OTM?</h2>
-      <h4 className="global__tx-md  what__subTitle">
+      <h2 className="global__tx-mdl  what__title">What is OTM?</h2>
+      <h4 className="global__tx-sm  what__subTitle">
         OTM offers real-time insider information on buying and selling strength
       </h4>
       <div className="what__video">
@@ -22,7 +22,7 @@ export default function Whatis() {
         ></iframe>
       </div>
 
-      <h4 className="global__tx-md what__subTitle2">
+      <h4 className="global__tx-sm what__subTitle2">
         OTM tools will take your trading to the next level
       </h4>
 

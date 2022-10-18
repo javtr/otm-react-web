7430a7e09eda7ba4ed954307911f260ce2b7640b
javtr <javtriana.z@gmail.com>
2022-10-18 08:12:57 -0500

    footer

--- src/components/pure/footer.jsx
+++ src/components/pure/footer.jsx
@@ -5,16 +5,25 @@ export default function Footer() {
     <div className="footer  global__cont">
       <hr className="footer__linea"></hr>
 
-      <p className="footer__text">
-      Trading futures, forex and options contains considerable risk and is not for all investors. An investor can potentially lose all or more than the initial investment. Risk capital is money that can be lost without jeopardizing financial security or lifestyle. Only risk capital should be used for trading and only those with sufficient risk capital should consider trading. Past performance is not necessarily indicative of future results.
-      </p>
+      <div className="footer__container">
+        <div className="footer__textContainer">
+          <p className="footer__text">
+            Trading futures, forex and options contains considerable risk and is
+            not for all investors. An investor can potentially lose all or more
+            than the initial investment. Risk capital is money that can be lost
+            without jeopardizing financial security or lifestyle. Only risk
+            capital should be used for trading and only those with sufficient
+            risk capital should consider trading. Past performance is not
+            necessarily indicative of future results.
+          </p>
+        </div>
 
-      <h4 className="footer__subTitle">OTM Objective Method</h4>
-
-      <p className="footer__mail">info@otmtrading.com</p>
-
-      <p className="footer__rights">© OTM Trading 2022</p>
-
+        <div className="footer__mailContainer">
+          <h4 className="footer__subTitle">OTM Objective Method</h4>
+          <p className="footer__mail">info@otmtrading.com</p>
+          <p className="footer__rights">© OTM Trading 2022</p>
+        </div>
+      </div>
     </div>
   );
 }

ea575986b12d833eda510a72130f1f49e234ea8a
javtr <javtriana.z@gmail.com>
2023-01-10 16:04:40 -0500

    testimonios

--- src/assets/info/reviewsData.js
+++ src/assets/info/reviewsData.js
@@ -2,7 +2,7 @@ export const ReviewsDataEn = [
   {
     imgUrl: "./review/rev_fedver.jpeg",
     title: "Federico Verdejo",
-    video: "./rev_gustavo_loiz.mp4",
+    video: "./rev_federico_verd.mp4",
   },
   {
     imgUrl: "./review/rev_gusloi.jpeg",
@@ -12,7 +12,7 @@ export const ReviewsDataEn = [
   {
     imgUrl: "./review/rev_wilfra.jpeg",
     title: "William franco",
-    video: "./rev_gustavo_loiz.mp4",
+    video: "./rev_william_franc.mp4",
   }
 ];
 

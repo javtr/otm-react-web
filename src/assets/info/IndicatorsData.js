221a20c28c81810f5f9f235775c09109632e603d
javtr <javtriana.z@gmail.com>
2023-03-06 16:54:08 -0500

    otm-web

--- src/assets/info/IndicatorsData.js
+++ src/assets/info/IndicatorsData.js
@@ -219,80 +219,141 @@ export const IndicatorsDataEs = [
 
     txmb2: "2. Señales informativas: los algoritmos de OTM Delta Swing detectan anomalías, lo que significa que te permiten detectar eventos fuera de lo normal y así tener una ventaja en el mercado. El algoritmo detecta 3 tipos de anomalías y nos muestra una señal informativa al final de la onda.",
 
-    txtb0: "Te recomiendo ver el siguiente video sobre las señales y los tipos de anomalías que detecta OTM Delta Swing para que puedas entenderlo mejor y obtener el máximo provecho:    ",
+    txtb1: "Te recomiendo ver el siguiente video sobre las señales y los tipos de anomalías que detecta OTM Delta Swing para que puedas entenderlo mejor y obtener el máximo provecho:    ",
     
 
     lnkb1:["https://www.quantower.com/blog","OTM Delta Swing"]
 
-
-
-
   },
   {
-    img: "./indicators/OTM_MICRO.gif",
-    tit: "OTM Delta Micro",
+
     linkTit: "OTM Delta Micro",
     url: "otm-delta-micro",
-    txt0: "A reliable movement is characterized by consecutive candles and constant volume, however it is important to know its buying or selling strength (delta), since the price action can deceive us. Delta Micro allows you to group the delta by consecutive movements, i.e. candlesticks towards the same direction (1). In this way you will be able to differentiate consistent movements from non-consistent ones. Simple visual identification is vital to avoid missing good trades. Some options offered by this indicator are: ",
-    sub1: "Institutional volumes.",
-    txt1: "Easily visualize large buying or selling volumes. The box that is excessively large regarding others, shows the presence of institutional money (1). ",
-    sub2: "Reference thresholds",
-    txt2: "Graphically filter delta volume values to only focus on the most relevant movements (2).",
+
+    imgb: "./features/otm-delta-micro.jpg",
+    titb_1: "OTM Delta Micro",
+
+    txtb0: "Cuando hay un gran interés por parte de los compradores o vendedores en mover el precio en una dirección, se puede identificar fácilmente con este indicador. Con OTM Micro Delta podrás:",
+
+    txmb1: "1. Tener un umbral de referencia: esto es clave para que filtres los movimientos importantes de los que no lo son.",
+
+    txmb2: "2. Delta institucional: cuando vemos una gran 'caja verde' significa que hay una gran cantidad de compradores que quieren que el precio suba, a esto lo llamamos 'Momentum'.",
+
+    txtb1: "Para utilizar este indicador de la mejor manera posible, es importante que entiendas algunos conceptos clave, pero no te preocupes, ¡son muy fáciles de entender! Además, tenemos un vídeo donde te lo explicamos todo:    ",
+    
+
+    lnkb1:["https://www.quantower.com/blog","OTM Delta Micro"]
+
+
   },
   {
-    img: "./indicators/OTM_CUMULATIVE.gif",
-    tit: "OTM Delta Cumulative",
+
     linkTit: "OTM Delta Cumulative",
     url: "otm-delta-cumulative",
-    txt0: "When is a trend exhausted? Is the price overbought or oversold? Delta Cumulative provides information about the buying or selling pressure in a delta histogram. There, we can observe when a trend is stable or shows a transition in the opposite direction (2). ",
-    sub1: "Buying or selling climax",
-    txt1: "Excessive buying or selling volume can be a sign of a possible change in direction. By filtering the high values you can identify excessive buying or selling (climax) (1). ",
+
+    imgb: "./features/otm-delta-cumulative.jpg",
+    titb_1: "OTM Delta Cumulative",
+
+    txtb0: "Este indicador es un histograma, un histograma nos muestra gráficamente las características de un conjunto de datos, en este caso resume el comportamiento del delta. El delta es una medida de la fuerza compradora o vendedora en el mercado. Algunas aplicaciones prácticas son:",
+
+    txmb1: "1. Identificar transiciones de tendencia: podemos ver si una tendencia está llegando a su fin.",
+
+    txmb2: "2. Identificar tendencias definidas: si queremos comprar, es importante observar una gráfica verde que va en aumento, esto indica que hay una presión constante de compradores.",
+
+    txtb1: "De acuerdo al tipo de estrategia que tengas, hay algunos aspectos importantes a tener en cuenta de este indicador, por tanto, te invito a que profundices más sobre OTM Cumulative Delta en el siguiente vídeo:  ",
+    
+
+    lnkb1:["https://www.quantower.com/blog","OTM Cumulative Delta"]
+
+
   },
   {
-    img: "./indicators/OTM_DELTA_BAR.gif",
-    tit: "OTM Delta Bar",
     linkTit: "OTM Delta Bar",
     url: "otm-delta-bar",
-    txt0: "Obtain each bar's delta volumen information in real time, as well as the máximum and mínimum values (1).",
-    sub1: "Divergences:",
-    txt1: "By using the signal you will be able to identify when a bar is not consistent with its delta volume. If there is a bullish candle and its delta is negative, the indicator will show the signal (2). ",
-    sub2: "Reference thresholds",
-    txt2: "Do not look at the information on each bar, visually filter the highest volumes to pay attention to them (3).",
+
+    imgb: "./features/otm-delta-bar.jpg",
+    titb_1: "OTM Delta Bar",
+
+    txtb0: "Si deseas conocer el valor del delta de una barra de forma detallada, OTM Delta Bar es una herramienta muy útil y versátil que te presenta múltiples opciones:",
+
+    txmb1: "1. Delta acumulado de sesión: muestra el comportamiento del delta durante toda la sesión, como un gráfico de velas japonesas, pero del comportamiento del delta. Es útil para confirmar agotamientos y detectar posibles cambios en el mercado.    ",
+
+    txmb2: "2. Delta por barra 1: muestra los valores positivos y negativos en la misma dirección.  ",
+
+    txmb3: "3. Delta por barra 2: el delta positivo estará hacia arriba y el delta negativo hacia abajo.",
+
+    txmb4: "4. BID/ASK: muestra los valores de BID (ventas agresivas) y ASK (compras agresivas) de cada vela. ",
+
+    txmb5: "5. Volumen + Delta: las barras que se muestran son de volumen y el color representa el resultado del delta de la vela. La intensidad del color depende de lo grande que sea ese valor de delta en relación con su volumen.  ",
+
+    txmb6: "6. Footer volumen + delta: aquí puedes ver los valores numéricos de delta, volumen, BID y ASK, entre otros. ",
+
+
+
+    txtb1: "La versión de pago incluye las 6 diferentes configuraciones, hay una versión gratuita que puedes descargar que incluye la configuración #1 y #3. Puedes profundizar y descargar el indicador en el siguiente botón.     ",
+    
+
+    lnkb1:["https://www.quantower.com/blog","OTM Delta bar Free"]
   },
   {
-    img: "./indicators/OTM_PROFILE.gif",
-    tit: "OTM Volume Profile",
-    linkTit: "OTM Volume Profile",
-    url: "otm-delta-profile",
-    txt0: "Have you ever wondered why the price consolidates more at some price levels than others? This is because some levels are preferred by large institutions so the price stays longer there, generating consolidations. While in the less preferred zones, the price moves vertically and faster. By using OTM Volume Profile you can easily determine this: ",
-    sub1: "Most traded levels",
-    txt1: "Price levels where the price will consolidate, by Identifying the volume control center (VPOC) (2). Besides, two objective support and resistance levels such as the value area low (VAL) and high (VAH), (1). ",
-    sub2: "Less traded levels",
-    txt2: "Identify the least traded price levels of the session (low volume levels). These levels allow to detect possible big movements (3). ",
-    sub3: "Dynamic volume profile",
-    txt3: "Do you want to go back a few bars and analyze the volume profile? Do not worry, this profile will be recreated in real time. ",
+    linkTit: "OTM Volumne Profile",
+    url: "otm-volume-profile",
+
+    imgb: "./features/otm-profile.jpg",
+    titb_1: "OTM Volume Profile",
+
+    txtb0: "El perfil de volumen es una herramienta muy útil para entender el comportamiento de los participantes del mercado y encontrar zonas importantes donde el precio puede reaccionar. Aquí te explico algunos usos prácticos:    ",
+
+    txmb1: "1. VPOC (Volume point of control): es el nivel más negociado durante la sesión, funciona como un imán para el precio y puede indicar una zona de salida en lugar de entrada.    ",
+
+    txmb2: "2. Nodos de bajo volumen: son zonas donde el volumen de negociación es bajo y pueden servir como niveles de soporte o resistencia, lo que significa que el precio puede rebotar en estas zonas y ofrecer buenos puntos de entrada o salida.    ",
+
+    txmb3: "3. Área de valor (Value Area): se refiere al rango de precios donde se negocia el 70% del volumen total de la sesión. Cuando el precio llega al extremo del área de valor alto (VAH) o bajo (VAL), es posible que haya una reversión de tendencia, por lo que es importante prestar atención a estas zonas.    ",
+
+    txtb1: "Aprende más sobre OTM Volume Profile en el siguiente vídeo:    ",
+    
+
+    lnkb1:["https://www.quantower.com/blog","OTM Volume Profile"]
   },
   {
-    img: "./indicators/OTM_VWAP.gif",
-    tit: "OTM VWAP",
     linkTit: "OTM VWAP",
-    url: "otm-delta-vwap",
-    txt0: "Have you ever wondered why the price consolidates more at some price levels than others? This is because some levels are preferred by large institutions so the price stays longer there, generating consolidations. While in the less preferred zones, the price moves vertically and faster. By using OTM Volume Profile you can easily determine this: ",
-    sub1: "Most traded levels",
-    txt1: "Price levels where the price will consolidate, by Identifying the volume control center (VPOC) (2). Besides, two objective support and resistance levels such as the value area low (VAL) and high (VAH), (1). ",
-    sub2: "Less traded levels",
-    txt2: "Identify the least traded price levels of the session (low volume levels). These levels allow to detect possible big movements (3). ",
-    sub3: "Dynamic volume profile",
-    txt3: "Do you want to go back a few bars and analyze the volume profile? Do not worry, this profile will be recreated in real time. ",
+    url: "otm-wap",
+
+    imgb: "./features/otm-vwap.jpg",
+    titb_1: "OTM VWAP",
+
+    txtb0: "El VWAP es un indicador muy útil en el trading y su cálculo es bastante sencillo. El VWAP significa 'Precio medio ponderado por volumen' y es como una medía móvil que se construye con el volumen de negociación del mercado. Este indicador tiene varias líneas que pueden servir de referencia para nuestras operaciones:    ",
+
+    txmb1: "1. VWAP: Es el VWAP 'Central' o principal. Si el precio está por encima, lo podemos tomar como soporte y comprar cuando el precio llegue cerca de esta línea. Si el precio está por debajo, lo tomaremos como una resistencia.    ",
+
+    txmb2: "2. VWAP desviación 1: El VWAP tiene unas líneas llamadas desviaciones que replican el comportamiento de la línea central a una distancia determinada (desviación estándar), las tenemos por arriba y por debajo, esta es la desviación #1 inferior.    ",
+
+    txmb3: "3. VWAP desviación 2: A medida que el precio avanza con una dirección clara, cada vez se va extendiendo y posiblemente en los retrocesos no llegue al VWAP central. Aquí, las desviaciones nos pueden servir de referencia para la ubicación de nuestras operaciones.    ",
+
+    txmb3: "4. VWAP desviación 3: Una vez que observamos que el precio ha llegado a la desviación #3 del VWAP, significa que el precio está muy extendido y es posible que luego vengan retrocesos muy profundos o cambios de dirección. Podemos usar esta desviación como punto de salida de nuestras operaciones.  ",
+
+
+    lnkb1:["https://www.quantower.com/blog","OTM VWAP"]
   },
   {
-    img: "./ind_06.gif",
-    tit: "OTM BarTimer",
-    linkTit: "OTM BarTimer",
-    url: "otm-delta-bartimer",
-    txt0: "OTM BarTimer allows you to visualize the remaining time for the closing of a bar. For this, it uses a color scale that will work as a traffic light for your executions. By default, it will have a different color in function of the bar's creation time. ",
+    linkTit: "OTM Graphic Bar Timer",
+    url: "otm-bar-timer",
+
+    imgb: "./features/otm-bar-timer.jpg",
+    titb_1: "OTM Graphic Bar Timer",
+
+    txtb0: "Posiblemente conoces el indicador de NinjaTrader Bar timer, que te dice cuánto tiempo queda para el cierre de una vela. Este indicador tiene el mismo principio, la diferencia es que muestra en una barra con colores el avance que lleva la construcción de la vela:    ",
+
+    txmb1: "1. 25% restante: puedes poner el color que quieras, pero el color verde indica que está próximo a cerrarse la vela, esto te facilita tomar una decisión.  ",
+
+    txmb2: "2. 50% restante: no ha pasado más de la mitad del tiempo para el cierre de la vela, puede ser prematuro tomar una decisión, por eso el color amarillo.    ",
+
+    txmb3: "3. 75% restante: la vela hasta ahora se está formando, la información dentro de ella puede cambiar drásticamente antes del cierre, así que es mejor esperar un poco.    ",
+
+    txtb1: "Este indicador te ayuda a identificar fácilmente en qué etapa se encuentra la construcción de la vela, con esto esperas la confirmación adecuada para apretar el gatillo ",
+
+    lnkb1:["https://www.quantower.com/blog","OTM Graphic Bar Timer"]  
   },
-  //Estrategias
 
   {
     img: "./instalation_mockup.jpg",

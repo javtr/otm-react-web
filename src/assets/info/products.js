99278b990a7d22ee2faea7343e1452cb26647474
javtr <javtriana.z@gmail.com>
2023-03-30 08:13:27 -0500

    pay

--- src/assets/info/products.js
+++ src/assets/info/products.js
@@ -4,31 +4,37 @@ export const Products = [
     total: "1152",
     price: 497,
     img: "./pack_mini.png",
+    key: "DRQ55LSKMWNQN"
   },
   {
     product: "swing",
     price: 288,
     img: "./swing_icon.png",
+    key: "MR29F7KUP5GPC"
   },
   {
     product: "micro",
     price: 288,
     img: "./micro_icon.png",
+    key: ""
   },
   {
     product: "profile",
     price: 192,
     img: "./profile_icon.png",
+    key: ""
   },
   {
     product: "cumulative",
     price: 192,
     img: "./cumulative_icon.png",
+    key: ""
   },
   {
     product: "vwap",
     price: 192,
     img: "./vwap_icon.png",
+    key: ""
   },
 
 
--- src/components/pure/paymentOrder.jsx
+++ src/components/pure/paymentOrder.jsx
@@ -1,16 +1,21 @@
 import React, { useState, useEffect, useContext } from "react";
 import ReactDOM from "react-dom";
-const PayPalButton = paypal.Buttons.driver("react", { React, ReactDOM });
+// const PayPalButton = paypal.Buttons.driver("react", { React, ReactDOM });
 import { Products } from "../../assets/info/products";
 import paypalImg from "../../assets/img/pay.png"
 import { textEn, textEs } from "../../assets/text/payment.js";
 import LanguageContext from "../../context/langContext.js";
+import { useParams } from "react-router-dom";
+import paypal_button from "../../assets/img/paypal_button.png"
+
+
 
 
 
 export default function PaymentOrder({ productId }) {
   const { lang, setLang } = useContext(LanguageContext);
   const [text, setText] = useState(textEn);
+  const params = useParams();
 
   useEffect(() => {
     if (lang == "en") {
@@ -25,26 +30,32 @@ export default function PaymentOrder({ productId }) {
 
 
   
-  const style = {
-    layout: "vertical",
-    color: "blue"
-  };
+  // const style = {
+  //   layout: "vertical",
+  //   color: "blue"
+  // };
 
-  const createOrder = (data, actions) => {
-    return actions.order.create({
-      purchase_units: [
-        {
-          amount: {
-            value: "0.01",
-          },
-        },
-      ],
-    });
-  };
+  // const createOrder = (data, actions) => {
+  //   return actions.order.create({
+  //     purchase_units: [
+  //       {
+  //         amount: {
+  //           value: "0.01",
+  //         },
+  //       },
+  //     ],
+  //   });
+  // };
 
-  const onApprove = (data, actions) => {
-    return actions.order.capture();
-  };
+  // const onApprove = (data, actions) => {
+  //   return actions.order.capture();
+  // };
+
+console.log(params.id);
+
+console.log(Products[params.id].key);
+
+
 
   return (
     <div className="payment">
@@ -88,11 +99,20 @@ export default function PaymentOrder({ productId }) {
           <div className="payment__content--buttons--paypal">
             {/* boton paypal ------------------------------------ */}
 
-            <PayPalButton
+            {/* <PayPalButton
               style={style}
               createOrder={(data, actions) => createOrder(data, actions)}
               onApprove={(data, actions) => onApprove(data, actions)}
-            />
+            /> */}
+
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+              <input type="hidden" name="cmd" value="_s-xclick"></input>
+              <input type="hidden" name="hosted_button_id" value={Products[params.id].key}></input>
+              <input className="payment__content--buttons--paypal--img" type="image" src={paypal_button} border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"></input>
+              <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1"></img>
+            </form>
+
+
 
             {/* boton paypal ------------------------------------ */}
           </div>

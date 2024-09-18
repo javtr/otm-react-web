bd91d1d0e9175514f051e0a9555a2a424f171e1a
javtr <javtriana.z@gmail.com>
2024-09-18 16:42:58 -0500

    inst

---
+++ src/assets/text/installation.js
@@ -0,0 +1,43 @@
+export const textEn =
+  {
+    titg1: "1. Indicators Installation",
+    titg1_01: "Standart Installation",
+    subg1_01: "",
+    urlg1_01: "H1mja0bI2vY?list=PLIm0iS26oKOQgGG__9ELGElfHLDPDI-kI",
+    titg1_02: "Installation with Exceptions (Recommended)",
+    subg1_02: "In certain cases, antivirus software may identify files as false positives. To solve this problem, it is recommended to follow these steps:",
+    urlg1_02: "YT4Meqk3egM?list=PLIm0iS26oKOQgGG__9ELGElfHLDPDI-kI",
+    titg1_03: "Uninstall Indicators",
+    subg1_03: "",
+    urlg1_03: "YM2Ffjs64MI?list=PLIm0iS26oKOQgGG__9ELGElfHLDPDI-kI",
+    titg2: "2. Common Errors",
+    titg2_01: "Error: Indicator does not load - 'DLL Error'",
+    subg2_01: "The indicators do not appear on the chart, and a 'DLL Error' appears in the Log window.",
+    urlg2_01: "s2NjDTDXPYU?list=PLIm0iS26oKOQgGG__9ELGElfHLDPDI-kI",
+    titg2_02: "Error: Indicator does not load - 'NO DLL Error Produced'",
+    subg2_02: "The indicators do not appear on the chart, but no 'DLL Error' is produced.",
+    urlg2_02: "8ZxzfS035SI?list=PLIm0iS26oKOQgGG__9ELGElfHLDPDI-kI",
+  };
+
+
+
+export const textEs =
+  {
+    titg1:"1. Instalación de indicadores",
+    titg1_01:"Instalación normal",
+    subg1_01: "",
+    urlg1_01: "meZ48zcoBSk?list=PLIm0iS26oKOQNrpqmDwQXMPE9wTJis8cI",
+    titg1_02:"Instalación con excepciones (recomendada)",
+    subg1_02: "En ciertos casos, el software antivirus puede identificar los archivos como falsos positivos. Para solucionar este problema, se recomienda seguir estos pasos:",
+    urlg1_02: "Op382KhsIpk?list=PLIm0iS26oKOQNrpqmDwQXMPE9wTJis8cI",
+    titg1_03:"Desinstalar indicadores",
+    subg1_03: "",
+    urlg1_03: "LtkiFfPEQX4?list=PLIm0iS26oKOQNrpqmDwQXMPE9wTJis8cI",
+    titg2:"2. Errores Comunes",
+    titg2_01:"Error: El indicador no carga - 'Error DLL'",
+    subg2_01: "Los indicadores no se muestran en el gráfico y en la ventana de Log aparece un 'Error DLL'.",
+    urlg2_01: "dJqkmFge_Mw?list=PLIm0iS26oKOQNrpqmDwQXMPE9wTJis8cI",
+    titg2_02:"Error: El indicador no carga - 'NO produce Error DLL'",
+    subg2_02: "Los indicadores no se muestran en el gráfico, pero no se produce el 'Error DLL'.",
+    urlg2_02: "CU3v9RXiz68?list=PLIm0iS26oKOQNrpqmDwQXMPE9wTJis8cI",
+  };
--- src/pages/InstalationPage.jsx
+++ src/pages/InstalationPage.jsx
@@ -1,15 +1,100 @@
-import React from "react";
-import installVid from "../assets/video/Installation.mp4"
-
+import React, { useState, useEffect, useContext } from "react";
+import { textEn, textEs } from "../assets/text/installation.js";
+import LanguageContext from "../context/langContext.js";
 
 const InstalationPage = () => {
+  const { lang, setLang } = useContext(LanguageContext);
+  const [text, setText] = useState({});
+
+  useEffect(() => {
+    if (lang == "en") {
+      setText(textEn);
+    } else if (lang == "es") {
+      setText(textEs);
+    } else {
+      setText(textEn);
+    }
+  }, [lang]);
 
   return (
     <div className="installation">
-      <video controls>
-        <source src={installVid} type="video/mp4" />
-        Tu navegador no soporta el formato de video.
-      </video>
+      <h1 className="installation__tituloGen">{text.titg1}</h1>
+      <h2 className="installation__titulo">{text.titg1_01}</h2>
+      <p className="installation__subTitulo"></p>
+
+      <div className="installation__video">
+        <iframe
+          title="Error: The indicator does not load - NO DLL Error Produced"
+          src={`https://www.youtube.com/embed/${text.urlg1_01}`}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
+
+
+      <h2 className="installation__titulo">{text.titg1_02}</h2>
+      <p className="installation__subTitulo">{text.subg1_02}</p>
+
+      <div className="installation__video">
+        <iframe
+          title="Error: The indicator does not load - NO DLL Error Produced"
+          src={`https://www.youtube.com/embed/${text.urlg1_02}`}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
+
+      <h2 className="installation__titulo">{text.titg1_03}</h2>
+      <p className="installation__subTitulo">{text.subg1_03}</p>
+
+      <div className="installation__video">
+        <iframe
+          title="Error: The indicator does not load - NO DLL Error Produced"
+          src={`https://www.youtube.com/embed/${text.urlg1_03}`}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>      
+
+      <h1 className="installation__tituloGen">{text.titg2}</h1>
+
+      <h2 className="installation__titulo">{text.titg2_01}</h2>
+      <p className="installation__subTitulo">{text.subg2_01}</p>
+
+      <div className="installation__video">
+        <iframe
+          title="Error: The indicator does not load - NO DLL Error Produced"
+          src={`https://www.youtube.com/embed/${text.urlg2_01}`}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>     
+
+      <h2 className="installation__titulo">{text.titg2_02}</h2>
+      <p className="installation__subTitulo">{text.subg2_02}</p>
+
+      <div className="installation__video">
+        <iframe
+          title="Error: The indicator does not load - NO DLL Error Produced"
+          src={`https://www.youtube.com/embed/${text.urlg2_02}`}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>     
+
+
+
+
     </div>
   );
 };
